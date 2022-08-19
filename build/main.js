71ab85d1e46ad9fb6e01d48288063e6d6ff73e57
MinKyu Tae <alsrb8442@naver.com>
2022-08-19 22:51:59 +0900

    ✨ Implement to add new card item

--- build/main.js
+++ build/main.js
@@ -1,17 +1,31 @@
 "use strict";
 const btns = document.querySelectorAll(".header-btn");
+const form = document.querySelector(".form-container");
 const formPopup = document.querySelector(".form-box");
 const formCloseBtn = document.querySelector(".close-btn");
-const formSubmitBtn = document.querySelector("submit-btn");
+const formSubmitBtn = document.querySelector(".submit-btn");
 const formChange = document.querySelector(".form-input-change");
 const overlay = document.querySelector(".overlay");
-// type btnName = "image-btn" | "video-btn" | "note-btn" | "task-btn";
+const cardContainer = document.querySelector(".card-container");
+class btnState {
+    constructor() {
+        this.selectedBtn = null;
+    }
+    setBtn(btn) {
+        this.selectedBtn = btn;
+    }
+    getBtn() {
+        return this.selectedBtn;
+    }
+}
+const btnClass = new btnState();
 /*
   form 여는 함수
 */
 function openForm(e) {
     const target = e.target;
     const btn_name = target.classList[1];
+    btnClass.setBtn(btn_name);
     if (btn_name === "note-btn" || btn_name === "task-btn") {
         formChange.innerHTML = `
       <label for="body">Body</label>
@@ -33,6 +47,66 @@ function openForm(e) {
 function closeForm() {
     formPopup === null || formPopup === void 0 ? void 0 : formPopup.classList.add("invisible");
     overlay === null || overlay === void 0 ? void 0 : overlay.classList.add("invisible");
+    btnClass.setBtn(null);
+}
+/*
+  form 카드 생성 함수
+*/
+function createCard(title, sub) {
+    const newCard = document.createElement("li");
+    newCard.setAttribute("class", "card");
+    const curBtn = btnClass.getBtn();
+    console.log(curBtn);
+    if (curBtn === "note-btn") {
+        newCard.innerHTML = `
+      <div class="card-info">
+        <span class="title">${title}</span>
+        <p class="description">${sub}</p>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+    }
+    else if (curBtn === "image-btn") {
+        newCard.innerHTML = `
+      <div class="card-img-container">
+        <img
+          src=${sub}
+          alt="dummy-image"
+        />
+      </div>
+      <div class="card-info">
+        <span class="title">${title}</span>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+    }
+    else if (curBtn === "video-btn") {
+        newCard.innerHTML = `
+      <div class="card-iframe-container">
+        <iframe
+          width="560"
+          height="315"
+          src=${sub}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div class="card-info">
+        <span class="title">${title}</span>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+    }
+    cardContainer === null || cardContainer === void 0 ? void 0 : cardContainer.append(newCard);
+    closeForm();
 }
 btns.forEach((btn) => {
     btn.addEventListener("click", openForm);
@@ -40,4 +114,20 @@ btns.forEach((btn) => {
 overlay === null || overlay === void 0 ? void 0 : overlay.addEventListener("click", closeForm);
 formCloseBtn === null || formCloseBtn === void 0 ? void 0 : formCloseBtn.addEventListener("click", closeForm);
 // 추후 폼 제출로 수정
-formSubmitBtn === null || formSubmitBtn === void 0 ? void 0 : formSubmitBtn.addEventListener("click", closeForm);
+// formSubmitBtn?.addEventListener("click", createCard);
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input1 = document.querySelector(".form-input-title input");
+    const input2_label = document.querySelector(".form-input-change label");
+    const input2_text = input2_label.innerText;
+    let input2;
+    if (input2_text === "URL") {
+        input2 = document.querySelector(".form-input-change input");
+    }
+    else {
+        input2 = document.querySelector(".form-input-change textarea");
+    }
+    createCard(input1.value, input2.value);
+    // console.log(input1.value);
+    // console.log(input2.value);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 const btns: NodeListOf<HTMLElement> = document.querySelectorAll(".header-btn");
 
-const formPopup: HTMLElement | null = document.querySelector(".form-box");
-const formCloseBtn: HTMLElement | null = document.querySelector(".close-btn");
-const formSubmitBtn = document.querySelector("submit-btn");
+const form = document.querySelector(".form-container");
+const formPopup = document.querySelector(".form-box");
+const formCloseBtn = document.querySelector(".close-btn");
+const formSubmitBtn = document.querySelector(".submit-btn");
 const formChange = document.querySelector(".form-input-change");
 
-const overlay: HTMLElement | null = document.querySelector(".overlay");
+const overlay = document.querySelector(".overlay");
 
-// type btnName = "image-btn" | "video-btn" | "note-btn" | "task-btn";
+const cardContainer = document.querySelector(".card-container");
+
+type btnName = "image-btn" | "video-btn" | "note-btn" | "task-btn";
+
+class btnState {
+  selectedBtn: btnName | null = null;
+
+  setBtn(btn: btnName | null) {
+    this.selectedBtn = btn;
+  }
+
+  getBtn(): btnName | null {
+    return this.selectedBtn;
+  }
+}
+
+const btnClass = new btnState();
 
 /*
   form 여는 함수
@@ -15,6 +32,8 @@ const overlay: HTMLElement | null = document.querySelector(".overlay");
 function openForm(e: Event) {
   const target = e.target as Element;
   const btn_name: string = target.classList[1];
+
+  btnClass.setBtn(btn_name as btnName);
 
   if (btn_name === "note-btn" || btn_name === "task-btn") {
     formChange!.innerHTML = `
@@ -38,6 +57,71 @@ function openForm(e: Event) {
 function closeForm() {
   formPopup?.classList.add("invisible");
   overlay?.classList.add("invisible");
+
+  btnClass.setBtn(null);
+}
+
+/*
+  form 카드 생성 함수
+*/
+function createCard(title: string, sub: string) {
+  const newCard: HTMLElement = document.createElement("li");
+  newCard.setAttribute("class", "card");
+
+  const curBtn = btnClass.getBtn();
+
+  console.log(curBtn);
+
+  if (curBtn === "note-btn") {
+    newCard.innerHTML = `
+      <div class="card-info">
+        <span class="title">${title}</span>
+        <p class="description">${sub}</p>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+  } else if (curBtn === "image-btn") {
+    newCard.innerHTML = `
+      <div class="card-img-container">
+        <img
+          src=${sub}
+          alt="dummy-image"
+        />
+      </div>
+      <div class="card-info">
+        <span class="title">${title}</span>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+  } else if (curBtn === "video-btn") {
+    newCard.innerHTML = `
+      <div class="card-iframe-container">
+        <iframe
+          width="560"
+          height="315"
+          src=${sub}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div class="card-info">
+        <span class="title">${title}</span>
+      </div>
+      <div class="card-delete">
+        <button>X</button>
+      </div>
+    `;
+  }
+
+  cardContainer?.append(newCard);
+
+  closeForm();
 }
 
 btns.forEach((btn) => {
@@ -48,4 +132,34 @@ overlay?.addEventListener("click", closeForm);
 formCloseBtn?.addEventListener("click", closeForm);
 
 // 추후 폼 제출로 수정
-formSubmitBtn?.addEventListener("click", closeForm);
+// formSubmitBtn?.addEventListener("click", createCard);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const input1 = document.querySelector(
+    ".form-input-title input"
+  ) as HTMLInputElement;
+
+  const input2_label = document.querySelector(
+    ".form-input-change label"
+  ) as HTMLLabelElement;
+
+  const input2_text = input2_label.innerText;
+  let input2;
+
+  if (input2_text === "URL") {
+    input2 = document.querySelector(
+      ".form-input-change input"
+    ) as HTMLInputElement;
+  } else {
+    input2 = document.querySelector(
+      ".form-input-change textarea"
+    ) as HTMLTextAreaElement;
+  }
+
+  createCard(input1.value, input2.value);
+
+  // console.log(input1.value);
+  // console.log(input2.value);
+});
