032b10d914a4d9a32dca2cf5227ac8612e1eb849
MinKyu Tae <alsrb8442@naver.com>
2022-08-21 19:43:45 +0900

    ✨ Implement image component

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,14 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent("Image Title", "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg");
+        image.attachTo(appRoot, "beforeend");
+    }
+    addElement(liComponent) {
+        this.page.attachTo(liComponent);
     }
 }
 new App(document.querySelector(".document"));
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg"
+    );
+    image.attachTo(appRoot, "beforeend");
+  }
+
+  addElement(liComponent: HTMLElement) {
+    this.page.attachTo(liComponent);
   }
 }
 
